36a7df6699b21755152bc124f0ef14152dfab7dc
emiliojuni0r <emiliohamsyahjr@gmail.com>
2026-01-15 09:41:07 +0700

    update : change icon for track record and equipment general

--- src/components/bymyself/navigation/Sidebar.tsx
+++ src/components/bymyself/navigation/Sidebar.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 
 import { FaGear } from "react-icons/fa6";
 import { IoIosArrowDown } from "react-icons/io";
-import { FaCircle } from "react-icons/fa";
+import { FaCircle, FaToolbox } from "react-icons/fa";
 import { TbReceiptFilled } from "react-icons/tb";
 import { usePathname } from "next/navigation";
-import { MdSupervisorAccount } from "react-icons/md";
+import { MdSupervisorAccount, MdViewTimeline } from "react-icons/md";
 import { SlEnvolopeLetter } from "react-icons/sl";
 import { BiSolidPurchaseTag } from "react-icons/bi";
 import { RiFolderReceivedFill } from "react-icons/ri";
@@ -124,11 +124,14 @@ export default function CoolSidebar({ collapsed, onCollapseChange }: { collapsed
                         <p className="text-base">Employee</p>
                     </Link>
                     <Link href={"/admin/track-record"} className="flex flex-row items-center h-10 space-x-3 hover:contrast-90">
-                        <FaCircle className="w-[23px] h-[23px]" />
+                        {/* <FaCircle className="w-[23px] h-[23px]" /> */}
+                        <MdViewTimeline className="w-[23px] h-[23px]" />
                         <p className="text-base">Track Record</p>
                     </Link>
                     <Link href={"/admin/equipment-general"} className="flex flex-row items-center h-10 space-x-3 hover:contrast-90">
-                        <FaCircle className="w-[23px] h-[23px]" />
+                        {/* <FaCircle className="w-[23px] h-[23px]" /> */}
+                        <FaToolbox className="w-[23px] h-[23px]"/>
+
                         <p className="text-base">equipment general</p>
                     </Link>
                 </div>
